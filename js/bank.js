2379278368cd7855cffda27713ae0b925770585e
bsnun8 <bishnunath018@gmail.com>
2022-02-18 11:19:25 +0600

    Deposit, withdraw, balance area using grid layout

--- js/bank.js
+++ js/bank.js
@@ -1,10 +1,12 @@
 document.getElementById('login-submit').addEventListener
 ('click', function () {
+  // get user email
   const emailField = document.getElementById('user-email');
   const userEmail = emailField.value;
   // get user password
   const passwordField = document.getElementById('user-password');
   const userPassword = passwordField.value;
+  // check email and password
   if (userEmail == 'abc@gmail.com' && userPassword == 'admin123') {
     window.location.href = 'banking.html';
   }
